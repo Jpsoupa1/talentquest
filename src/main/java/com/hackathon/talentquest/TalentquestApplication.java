a8f8ef8d4f8bdda62ddb57c4c06175e9dc6783f7
Jpsoupa1 <joaopedrosoupa@sempreceub.com>
2025-12-10 16:42:55 -0300

    first commit

---
+++ src/main/java/com/hackathon/talentquest/TalentquestApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.talentquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/talentquest/TalentquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.talentquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
